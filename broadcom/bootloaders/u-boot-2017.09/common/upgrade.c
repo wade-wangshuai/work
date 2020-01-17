d298dda8cbda3a81f360674c64f6b46cd582d016
wade <wade_wang@sdc.sercomm.com>
2020-01-17 09:31:17 +0800

    upgrade is ok,Two dimensional arrays are used

--- broadcom/bootloaders/u-boot-2017.09/common/upgrade.c
+++ broadcom/bootloaders/u-boot-2017.09/common/upgrade.c
@@ -4,7 +4,7 @@
 static int do_upgrade(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 
-    char cmd[200][200]={"setenv ipaddr 172.21.53.221;setenv serverip 172.21.53.45\n","tftpboot 0x10000000 bcm947622EAP_nand_cferom_fs_image_128_ubi.w\n","setenv calsize 0x580000;setexpr leftsize $filesize - $calsize;echo $leftsize\n","nand erase 0 0x300000;nand erase 0x580000 0x7A80000","nand write 0x10000000 0x0000 0x200000; nand write 0x10580000 0x580000 $leftsize"};
+    char cmd[5][100]={"setenv ipaddr 172.21.53.221;setenv serverip 172.21.53.45\n","tftpboot 0x10000000 bcm947622EAP_nand_cferom_fs_image_128_ubi.w\n","setenv calsize 0x580000;setexpr leftsize $filesize - $calsize;echo $leftsize\n","nand erase 0 0x300000;nand erase 0x580000 0x7A80000","nand write 0x10000000 0x0000 0x200000; nand write 0x10580000 0x580000 $leftsize"};
 	int i;
 	for (i = 0; i < 5; i++) 
 	{
