434b225e2cd797ea57909ecb9baffe199e47ebf5
wade <wade_wang@sdc.sercomm.com>
2020-01-17 09:21:35 +0800

    upgrade is ok,Two dimensional arrays are used

--- broadcom/bootloaders/u-boot-2017.09/common/upgrade.c
+++ broadcom/bootloaders/u-boot-2017.09/common/upgrade.c
@@ -4,39 +4,13 @@
 static int do_upgrade(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 
-//	char cmd_buf[200][200];
     char cmd[200][200]={"setenv ipaddr 172.21.53.221;setenv serverip 172.21.53.45\n","tftpboot 0x10000000 bcm947622EAP_nand_cferom_fs_image_128_ubi.w\n","setenv calsize 0x580000;setexpr leftsize $filesize - $calsize;echo $leftsize\n","nand erase 0 0x300000;nand erase 0x580000 0x7A80000","nand write 0x10000000 0x0000 0x200000; nand write 0x10580000 0x580000 $leftsize"};
 	int i;
 	for (i = 0; i < 5; i++) 
 	{
-	//puts(cmd[i]);
-//	strncpy(cmd_buf[i],cmd[i],100);
-//	run_command(cmd_buf[i], 0);
-	run_command(cmd[i], 0);
+		run_command(cmd[i], 0);
 	}
 
-
-
-
-
-/*	
-	char cmd_buf[200];
-	strcpy(cmd_buf, "setenv ipaddr 172.21.53.221;setenv serverip 172.21.53.45");
-	run_command(cmd_buf, 0);
-
-	strcpy(cmd_buf,"tftpboot 0x10000000 bcm947622EAP_nand_cferom_fs_image_128_ubi.w"); 
-	run_command(cmd_buf, 0);
-
-	strcpy(cmd_buf,"setenv calsize 0x580000;setexpr leftsize $filesize - $calsize;echo $leftsize"); 
-	run_command(cmd_buf, 0);
-	
-	strcpy(cmd_buf,"nand erase 0 0x300000;nand erase 0x580000 0x7A80000\n"); 
-	run_command(cmd_buf, 0);
-	
-	strcpy(cmd_buf,"nand write 0x10000000 0x0000 0x200000; nand write 0x10580000 0x580000 $leftsize\n"); 
-	run_command(cmd_buf, 0);
-*/
-			return 0;
 }
 
 U_BOOT_CMD(
